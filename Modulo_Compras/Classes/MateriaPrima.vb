0a8c58917ce545b50602b0bed4c45abf77865d39
ceruttigabriel <ceruttigabriel@82fc2040-4b34-0410-ae03-fda77b61a599>
2007-08-07 03:39:49 +0000

    Clase MateriaPrima creada

---
+++ Modulo_Compras/Classes/MateriaPrima.vb
@@ -0,0 +1,31 @@
+
+
+Public Class MateriaPrima
+
+    Private selectMateriaPrima As New SqlClient.SqlCommand
+    Private adaptadorMateriaPrima As New SqlClient.SqlDataAdapter
+    Private resultado As SqlClient.SqlDataReader
+
+    Public Function obtenerPrecioMateriaPrima(ByVal idMateriPrima As Integer) As Double
+        selectMateriaPrima.Connection = cnn
+        selectMateriaPrima.Connection.Open()
+        selectMateriaPrima.CommandText = "SELECT tmp.idtipomateriaprima, tmp.nombre, doc.precio, MAX(ocmp.fecharealizacion) " & _
+                "FROM tipomateriaprima tmp, detalleordencompra doc, ordencompramp ocmp " & _
+                "WHERE(tmp.idtipomateriaprima = doc.idtipomateriaprima) " & _
+                "AND doc.idordencompra = ocmp.idordencompra " & _
+                "AND doc.idestado IN (12, 13) " & _
+                "AND tmp.idtipomateriaprima = " + idMateriPrima.ToString & _
+                "GROUP BY tmp.idtipomateriaprima, tmp.nombre, doc.precio, ocmp.fecharealizacion"
+
+        selectMateriaPrima.CommandType = CommandType.Text
+        resultado = selectMateriaPrima.ExecuteReader
+        Dim precio As Double
+        While resultado.Read
+            precio = resultado.Item(2)
+        End While
+
+        Return precio
+
+    End Function
+
+End Class
