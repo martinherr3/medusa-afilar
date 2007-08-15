10848fc864f101eebf3aefd01c2825bf92704482
ceruttigabriel <ceruttigabriel@82fc2040-4b34-0410-ae03-fda77b61a599>
2007-08-15 03:31:18 +0000

    Agregado de constantes de estado de modelo de fresa

--- trunk/Modulo_Sistema/Classes/Constantes.vb
+++ trunk/Modulo_Sistema/Classes/Constantes.vb
@@ -1,10 +1,14 @@
 Public Class Constantes
+
     Public Const HORA_HOMBRE_OPERARIO As String = "Hora/Hombre Operario"
+    Public Const MODELO_FRESA_ESTADO_BAJA As String = "Baja"
+    Public Const MODELO_FRESA_ESTADO_ALTA As String = "Alta"
 
     'setea el inicio de las coordenadas X e Y de los formularios, las que heredan
     'de frmBase
     Private _InicioX As Integer = 200
     Private _InicioY As Integer = 105
+
 
     Public ReadOnly Property InicioX() As Integer
         Get
@@ -12,13 +16,11 @@ Public Class Constantes
         End Get
     End Property
 
+
     Public ReadOnly Property InicioY() As Integer
         Get
             InicioY = _InicioY
         End Get
     End Property
 
-
-    'Public Const InicioX As Integer = 200
-    'Public Const InicioY As Integer = 105
 End Class
