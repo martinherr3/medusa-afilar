56cae4ebc6006a9ee463fcc9b983999e1ef6d85e
ceruttigabriel <ceruttigabriel@82fc2040-4b34-0410-ae03-fda77b61a599>
2007-08-28 02:34:25 +0000

    + Funciones de imagen

--- trunk/ModuloPrincipal.vb
+++ trunk/ModuloPrincipal.vb
@@ -14,19 +14,16 @@ Module ModuloPrincipal
 
     Public cnn As New SqlClient.SqlConnection
     Public princ As New principal
-    'Public SQLBD As New frmBaseDatos
     Public seguridad As New ClsSeg
 
 
     Sub main()
 
-        'cnn.ConnectionString = "workstation id=GERARDO;packet size=4096;integrated security=SSPI;initial catalog=Afilar;persist security info=False"
         Dim oBaseDatos As New BaseDatos
         cnn.ConnectionString = oBaseDatos.GetConection()
         oBaseDatos = Nothing
 
         System.Windows.Forms.Application.Run(princ)
-        'System.Windows.Forms.Application.Run(SQLBD)
         
     End Sub
 
@@ -337,6 +334,34 @@ Module ModuloPrincipal
         Dim projectPath As String
         projectPath = AppSettings.Get("appPath")
         Return projectPath
+    End Function
+
+
+    Public Function Image2Bytes(ByVal img As Image) As Byte()
+        Dim sTemp As String = IO.Path.GetTempFileName()
+        Dim fs As New IO.FileStream(sTemp, IO.FileMode.OpenOrCreate, IO.FileAccess.ReadWrite)
+        img.Save(fs, System.Drawing.Imaging.ImageFormat.Png)
+        fs.Position = 0
+        '
+        Dim imgLength As Integer = CInt(fs.Length)
+        Dim bytes(0 To imgLength - 1) As Byte
+        fs.Read(bytes, 0, imgLength)
+        fs.Close()
+        Return bytes
+    End Function
+
+
+    Public Function Bytes2Image(ByVal bytes() As Byte) As Image
+        If bytes Is Nothing Then Return Nothing
+        '
+        Dim ms As New IO.MemoryStream(bytes)
+        Dim bm As Bitmap = Nothing
+        Try
+            bm = New Bitmap(ms)
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.Message)
+        End Try
+        Return bm
     End Function
 
 End Module
