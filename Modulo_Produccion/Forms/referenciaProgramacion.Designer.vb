9bbe0e1d577b6e310344e5f207d98c75fa3f1591
ceruttigabriel <ceruttigabriel@82fc2040-4b34-0410-ae03-fda77b61a599>
2007-11-24 20:54:19 +0000

    Agregado boton en barra de abajo, ubicacion form y tamaño

--- Modulo_Produccion/Forms/referenciaProgramacion.Designer.vb
+++ Modulo_Produccion/Forms/referenciaProgramacion.Designer.vb
@@ -19,43 +19,49 @@ Partial Class referenciaProgramacion
   'No lo modifique con el editor de código.
   <System.Diagnostics.DebuggerStepThrough()> _
   Private Sub InitializeComponent()
-    Me.TreeProgramacion = New System.Windows.Forms.TreeView
-    Me.UltraButton1 = New Infragistics.Win.Misc.UltraButton
-    Me.SuspendLayout()
-    '
-    'TreeProgramacion
-    '
-    Me.TreeProgramacion.Location = New System.Drawing.Point(9, 12)
-    Me.TreeProgramacion.Name = "TreeProgramacion"
-    Me.TreeProgramacion.Size = New System.Drawing.Size(393, 284)
-    Me.TreeProgramacion.TabIndex = 10
-    '
-    'UltraButton1
-    '
-    Me.UltraButton1.Location = New System.Drawing.Point(304, 301)
-    Me.UltraButton1.Name = "UltraButton1"
-    Me.UltraButton1.Size = New System.Drawing.Size(96, 19)
-    Me.UltraButton1.TabIndex = 11
-    Me.UltraButton1.Text = "Cerrar"
-    '
-    'referenciaProgramacion
-    '
-    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-    Me.ClientSize = New System.Drawing.Size(409, 325)
-    Me.Controls.Add(Me.UltraButton1)
-    Me.Controls.Add(Me.TreeProgramacion)
-    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-    Me.MaximizeBox = False
-    Me.MinimizeBox = False
-    Me.Name = "referenciaProgramacion"
-    Me.ShowIcon = False
-    Me.ShowInTaskbar = False
-    Me.Text = "Referencias"
-    Me.TopMost = True
-    Me.ResumeLayout(False)
+        Dim Appearance1 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(referenciaProgramacion))
+        Me.TreeProgramacion = New System.Windows.Forms.TreeView
+        Me.UltraButton1 = New Infragistics.Win.Misc.UltraButton
+        Me.SuspendLayout()
+        '
+        'TreeProgramacion
+        '
+        Me.TreeProgramacion.Location = New System.Drawing.Point(9, 12)
+        Me.TreeProgramacion.Name = "TreeProgramacion"
+        Me.TreeProgramacion.Size = New System.Drawing.Size(393, 284)
+        Me.TreeProgramacion.TabIndex = 10
+        '
+        'UltraButton1
+        '
+        Appearance1.Image = CType(resources.GetObject("Appearance1.Image"), Object)
+        Me.UltraButton1.Appearance = Appearance1
+        Me.UltraButton1.Location = New System.Drawing.Point(306, 302)
+        Me.UltraButton1.Name = "UltraButton1"
+        Me.UltraButton1.Size = New System.Drawing.Size(96, 31)
+        Me.UltraButton1.TabIndex = 11
+        Me.UltraButton1.Text = "&Salir"
+        '
+        'referenciaProgramacion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 339)
+        Me.Controls.Add(Me.UltraButton1)
+        Me.Controls.Add(Me.TreeProgramacion)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Location = New System.Drawing.Point(200, 106)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "referenciaProgramacion"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Referencias"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
 
-  End Sub
+    End Sub
   Friend WithEvents TreeProgramacion As System.Windows.Forms.TreeView
   Friend WithEvents UltraButton1 As Infragistics.Win.Misc.UltraButton
 End Class
